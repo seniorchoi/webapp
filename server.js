d06cf75168ccc80313de58cb737cc972209236cb
Seungho Choi <ksngho7@gmail.com>
2018-07-21 16:47:05 +0900

    a

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next)=>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log+ '\n', err=>{
+    if(err){
+      console.log('gg');
+    }
+  })
+  next();
+});
+
+// app.use((req, res, next)=> {
+//   res.render('maintenance.hbs')
+// })
+
+hbs.registerHelper('getCurrentYear', ()=> {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt',text=>{
+  return text.toUpperCase();
+})
+
+app.get('/',(req, res) => {
+  res.render('home.hbs',{
+    pageTitle:'Home page',
+    welcomeMessage:'welcome baby'
+  });
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:'about page'
+  });
+});
+
+app.get('/bad',(req, res) => {
+  res.send({
+    errorMessage: 'unable to handle my big Dick'
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, ()=>{
+  console.log(`server is up on port ${port} baby`);
+});
