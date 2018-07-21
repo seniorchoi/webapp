0943c80365f01dab5e9ea0e183fe0ef139711049
Seungho Choi <ksngho7@gmail.com>
2018-07-21 17:04:32 +0900

    a

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/sexy',(req, res) => {
   });
 });
 
+app.get('/projects',(req, res) => {
+  res.render('projects.hbs',{
+    pageTitle:'Projects'
+  });
+});
+
 app.get('/about',(req,res)=>{
   res.render('about.hbs',{
     pageTitle:'about page'
